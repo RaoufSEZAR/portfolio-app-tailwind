8122c81702b40f8bd361e9f278db8b87e02cbaec
raoufsezar <raoufsato999@gmail,com>
2022-10-09 14:15:31 +0300

    add dark mode feature

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const hamberger = document.querySelector("#hamberger");
 const menu = document.querySelector("#menu");
+const body = document.querySelector("body");
+const moon = document.querySelector("#moon");
+const hLink = document.querySelectorAll(".hLink");
 
 hamberger.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
 	hamberger.classList.toggle("bg-white");
+});
+moon.addEventListener("click", () => {
+	body.classList.toggle("dark");
+});
+hLink.forEach((link) => {
+	link.addEventListener("click", () => {
+		menu.classList.toggle("hidden");
+		hamberger.classList.toggle("bg-white");
+	});
 });
